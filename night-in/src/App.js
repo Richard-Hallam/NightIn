8902c0c4d552a4b03f991903d2eb17312fa5392f
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-11-21 20:38:48 +0000

    Began making the listed movies component.

--- night-in/src/App.js
+++ night-in/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './styles/App.css';
 import Navbar from './components/navbar';
 import Title from './components/title';
+import Results from './components/Results';
+
 function App() {
   return (
     <div className="homepage">
       <Navbar/>
       <Title />
+      <Results/>
     </div>
   );
 }
---
+++ night-in/src/components/Results.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/results.css';
+
+function Results() {
+    return (
+        <div className="results">
+            <div className="match-button">
+                {/* Content for match button */}
+            </div>
+            <div className="results-height-div">
+                {/* Content for the div that takes up 100vh */}
+            </div>
+            <p>lorem font-family: 'Abril Fatface', serif;font-family: 'Abril Fatface', serif;</p>
+            {/* Other content */}
+        </div>
+    );
+}
+
+export default Results;
