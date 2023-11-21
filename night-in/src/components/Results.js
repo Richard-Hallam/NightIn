b87ccd10e7c551b4f4bb77079599852845baefd8
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-11-21 20:49:36 +0000

    Added the div ready to take the component.

--- night-in/src/components/Results.js
+++ night-in/src/components/Results.js
@@ -4,14 +4,10 @@ import '../styles/results.css';
 function Results() {
     return (
         <div className="results">
-            <div className="match-button">
-                {/* Content for match button */}
+    
+            <div className="results-div">
+                
             </div>
-            <div className="results-height-div">
-                {/* Content for the div that takes up 100vh */}
-            </div>
-            <p>lorem font-family: 'Abril Fatface', serif;font-family: 'Abril Fatface', serif;</p>
-            {/* Other content */}
         </div>
     );
 }
