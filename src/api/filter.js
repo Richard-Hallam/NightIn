8f5dd105f5690e7d3aa2631112075235acebf56e
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2023-11-28 10:10:54 +0000

    created filter.js

---
+++ src/api/filter.js
@@ -0,0 +1,8 @@
+import getMoviesByGenre from "./getMoviesByGenre";
+
+
+function filter(genres, vote_average_cut_off, release_date){
+    let film_list = getMoviesByGenre(genres[0]);
+
+    film_list
+}
